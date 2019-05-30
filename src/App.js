25c02d1d32c4e202dc2780131b5828217f73e9a4
Kevin <KJNoonan52@gmail.com>
2019-05-29 21:21:35 -0500

    Created a react app and got css modules working

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./App.module.scss";
+
+const App = () => {
+  console.log(styles);
+  return <div className={styles.App}>Hello, I'm Kevin and I build things</div>;
+};
+
+export default App;
