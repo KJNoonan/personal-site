f9f8e7429137f0aa649f99d1ef96104f1cfe7259
KJNoonan52@gmail.com <KJNoonan52@gmail.com>
2019-06-03 20:59:31 -0500

    Adding basic Main Page

---
+++ src/js/Main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Main.module.scss";
+
+const Main = () => {
+  return <div className={styles.App}>Hello, I'm Kevin and I build things</div>;
+};
+
+export default Main;
