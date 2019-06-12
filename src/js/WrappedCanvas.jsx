4e323d543474218bc8f374fc4b534347257f4155
KJNoonan52@gmail.com <KJNoonan52@gmail.com>
2019-06-11 20:19:07 -0500

    Created the meteor animation

--- src/js/WrappedCanvas.jsx
+++ src/js/WrappedCanvas.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useLayoutEffect } from "react";
-import { drawMountain } from "./canvasMountainUtils.js";
+import { drawMountain, spawnMeteor } from "./canvasMountainUtils.js";
 
 const draw = (ctx, width, height) => {
   drawMountain(ctx, width, height);
@@ -8,12 +8,22 @@ const draw = (ctx, width, height) => {
 const WrappedCanvas = ({ width, height }) => {
   const canvas = useRef(null);
 
+  const onClickSpawnMeteor = event => {
+    console.log("clicked");
+    const context = canvas.current.getContext("2d");
+    const width = canvas.current.width;
+    const height = canvas.current.height;
+
+    console.log(event.pageX, event.pageY);
+    spawnMeteor(context, event.pageX, event.pageY, width, height)();
+  };
+
   useLayoutEffect(() => {
     const context = canvas.current.getContext("2d");
     const width = canvas.current.width;
     const height = canvas.current.height;
-    console.log(context, width, height);
     draw(context, width, height);
+    window.addEventListener("click", onClickSpawnMeteor);
   });
 
   return <canvas ref={canvas} width={width} height={height} />;
--- src/js/canvasMountainUtils.js
+++ src/js/canvasMountainUtils.js
@@ -25,3 +25,29 @@ export const drawMountain = (ctx, width, height) => {
   ctx.stroke();
   ctx.closePath();
 };
+
+const SIZE_OF_METOR = 5;
+const DROP_SPEED = 6;
+export const spawnMeteor = (ctx, x, y, width, height) => {
+  console.log(x, y);
+  let spawningX = Math.random() * (width - SIZE_OF_METOR);
+  let spawningY = -1 * SIZE_OF_METOR;
+  const slope = (y - spawningY) / (x - spawningX);
+  const yIntercept = -1 * slope * x + y;
+  const animate = () => {
+    ctx.beginPath();
+    ctx.clearRect(0, 0, width, height);
+    ctx.arc(spawningX, spawningY, SIZE_OF_METOR, 0, 2 * Math.PI);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.closePath();
+    drawMountain(ctx, width, height);
+
+    //begin calculations for the next draw
+    spawningY = spawningY + DROP_SPEED;
+    spawningX = (spawningY - yIntercept) / slope;
+    console.log(spawningX, spawningY);
+    requestAnimationFrame(animate);
+  };
+  return animate;
+};
